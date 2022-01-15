6485cd97854920cb60de3671c7bd533e689efee5
Cody <Mason.Cody.M@gmail.com>
2022-01-14 19:22:39 -0500

    Implemented ValidatePackagedFiles.

--- src/core-impls/TR3InstallationManager.cs
+++ src/core-impls/TR3InstallationManager.cs
@@ -19,7 +19,8 @@ namespace TR3_Version_Swapper
 
         protected override void ValidatePackagedFiles()
         {
-            throw new System.NotImplementedException();
+            // Check that each version's game files are present and unmodified.
+            ValidateMd5Hashes(TR3FileAudit.VersionFilesAudit, Directories.Versions);
         }
     }
 }
